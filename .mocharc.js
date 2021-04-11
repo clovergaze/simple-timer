3bc815d37b16880b7d995fea02fcca33fe989940
Johannes Hillert <johannes.hillert@infokin.org>
2021-04-11 19:45:14 +0200

    migrated Mocha configuration

---
+++ .mocharc.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+
+    extension: [
+        "ts"
+    ],
+    exclude: [
+        "test/*.d.ts"
+    ],
+    require: [
+        "ts-node/register"
+    ],
+    timeout: 10000
+
+};
